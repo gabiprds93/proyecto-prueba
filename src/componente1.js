b00993a118641ca75d3e0f96b2359de586bc9ffa
gabiprds93 <gabiprds.93@gmail.com>
2017-10-09 17:07:25 -0500

    Componente1

---
+++ src/componente1.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class component1 extends Component {
+  render() {
+    return (
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+    );
+  }
+}
+
+export default component1;
